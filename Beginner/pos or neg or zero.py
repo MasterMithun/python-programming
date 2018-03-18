f8b1b89913b110b3887db17b8eb04f3568f1f68e
Mithun <37336341+MasterMithun@users.noreply.github.com>
2018-03-18 06:29:21 +0000

    Update pos or neg or zero.py

--- Beginner/pos or neg or zero.py
+++ Beginner/pos or neg or zero.py
@@ -8,3 +8,11 @@ try:
         print("The given interger is ne")
 except:        
     print("invalid entry")
+    
+b=input("enter the input:")
+if (int(b)>0):
+    print("the given is positive")
+elif (int(b)==0):
+    print("the given is zero")
+else:
+    print("the given is negative")
