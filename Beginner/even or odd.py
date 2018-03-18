90e071fb4e99ac098d69b31d2dddc094c0366ee5
Mithun <37336341+MasterMithun@users.noreply.github.com>
2018-03-18 16:24:39 +0530

    Update even or odd.py

--- Beginner/even or odd.py
+++ Beginner/even or odd.py
@@ -1,6 +1,6 @@
 b= input("enter the input:")
 if b.isdigit():
-    if (int(b)%2==0):
+    if (float(b)%2==0):
         print("The given number is even")
     else:
         print("The given interger is odd")
