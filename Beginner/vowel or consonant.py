986fe6f21340b2bf8e29be315f6149e1f135405c
Mithun <37336341+MasterMithun@users.noreply.github.com>
2018-03-20 11:32:38 +0530

    Create vowel or consonant.py

---
+++ Beginner/vowel or consonant.py
@@ -0,0 +1,12 @@
+c=input('enter the input:')
+list1=['a','e','i','o','u',]
+list2=['A','E','I','O','U']
+c=c.lower()
+c=c.upper()
+if c in list1:
+	print("vowels")
+elif c in list2:
+  print("vowels")
+else:
+    print("consonant")     
+		
