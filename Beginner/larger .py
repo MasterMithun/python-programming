445fd62ccedc3e01cd98e78d124b5243e4563184
Mithun <37336341+MasterMithun@users.noreply.github.com>
2018-03-20 11:49:56 +0530

    Create larger .py

---
+++ Beginner/larger .py
@@ -0,0 +1,12 @@
+a=int(input('enter the input:'))
+b=int(input('enter the input:'))
+c=int(input('enter the input:'))
+if (a>=b):
+  print('a is larger')
+elif (b>=c):
+  print('b is larger')
+else:
+  print('c is larger')
+
+     
+    
