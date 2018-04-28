e3cd595e24c0c20ecb0664542efb7429ab22d717
Mithun <37336341+MasterMithun@users.noreply.github.com>
2018-04-28 10:00:04 +0000

    Update interval odd value print .py

--- Beginner/interval odd value print .py
+++ Beginner/interval odd value print .py
@@ -1,30 +1,30 @@
-i=input().split()
-i=list(map(int,i))
-if(i[0]%2==0):
-  g=i[0]+1
-  while(g<i[1]):
-    if(i[1]%2==0):
-      if(i[1]==g+1):
-        print(g,end='')
+j=input().split()
+j=list(map(int,j))
+if(j[0]%2==0):
+  k=j[0]+1
+  while(k<j[1]):
+    if(j[1]%2==0):
+      if(j[1]==k+1):
+        print(k,end='')
       else:
-        print(g,end=' ')
+        print(k,end=' ')
     else:
-      if(i[1]==g+2):
-        print(g,end='')
+      if(j[1]==k+2):
+        print(k,end='')
       else:
-        print(g,end=' ')
-    g=g+2
-elif(i[0]%2!=0):
-  g=i[0]+2
-  while(g<i[1]):
-    if(i[1]%2==0):
-      if(i[1]==g+1):
-        print(g,end='')
+        print(k,end=' ')
+    k=k+2
+elif(j[0]%2!=0):
+  k=j[0]+2
+  while(k<j[1]):
+    if(j[1]%2==0):
+      if(j[1]==k+1):
+        print(k,end='')
       else:
-          print(g,end=' ')
+          print(k,end=' ')
     else:
-      if(i[1]==g+2):
-        print(g,end='')
+      if(j[1]==k+2):
+        print(k,end='')
       else:
-        print(g,end=' ')
-    g=g+2
+        print(k,end=' ')
+    k=k+2
