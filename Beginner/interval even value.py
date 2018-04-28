fe62986ddee4493acfb589ab81d9ebf48ce5ef1a
Mithun <37336341+MasterMithun@users.noreply.github.com>
2018-04-28 10:12:38 +0000

    Create interval even value.py

---
+++ Beginner/interval even value.py
@@ -0,0 +1,30 @@
+j=input().split()
+j=list(map(int,j))
+if(j[0]%2==0):
+  k=j[0]+2
+  while(k<j[1]):
+    if(j[1]%2!=0):
+      if(j[1]==k+1):
+        print(k,end='')
+      else:
+        print(k,end=' ')
+    else:
+      if(j[1]==k+2):
+        print(k,end='')
+      else:
+        print(k,end=' ')
+    k=k+2
+elif(j[0]%2!=0):
+  k=j[0]+1
+  while(k<j[1]):
+    if(j[1]%2!=0):
+      if(j[1]==k+1):
+        print(k,end='')
+      else:
+          print(k,end=' ')
+    else:
+      if(j[1]==k+2):
+        print(k,end='')
+      else:
+        print(k,end=' ')
+    k=k+2
